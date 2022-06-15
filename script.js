53a45f71af9a7f6704502725c4dcda863d70c26b
ryanmpdev <ryanmaciel697@gmail.com>
2022-06-15 11:55:49 -0300

    primeiro desafio js

---
+++ script.js
@@ -0,0 +1,16 @@
+let MinhaCachorra = 'Safira';
+
+let idade = 16;
+
+let ano = 2022;
+
+console.log(ano - idade);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = false;
+
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
+    
